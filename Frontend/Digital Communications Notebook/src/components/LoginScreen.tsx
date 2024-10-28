b745b31f8a2aa1488e3e206bce38b458c734bb02
GabrielAlfaro07 <galfaulate@gmail.com>
2024-10-28 00:47:17 -0600

    chore: changed the input's sizes

--- Frontend/Digital Communications Notebook/src/components/LoginScreen.tsx
+++ Frontend/Digital Communications Notebook/src/components/LoginScreen.tsx
@@ -1,16 +1,16 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 
 interface LoginScreenProps {
   onRegisterClick: () => void;
 }
 
 const LoginScreen: React.FC<LoginScreenProps> = ({ onRegisterClick }) => {
-  const [email, setEmail] = useState<string>('');
-  const [password, setPassword] = useState<string>('');
+  const [email, setEmail] = useState<string>("");
+  const [password, setPassword] = useState<string>("");
 
   const handleLogin = () => {
     // Lógica para manejar el login
-    console.log('Login:', { email, password });
+    console.log("Login:", { email, password });
   };
 
   return (
@@ -18,19 +18,22 @@ const LoginScreen: React.FC<LoginScreenProps> = ({ onRegisterClick }) => {
       <h2 className="text-3xl font-bold mb-6">Login</h2>
       <input
         type="email"
-        className="p-2 mb-4 border rounded w-64"
+        className="p-2 mb-4 border rounded w-80"
         placeholder="Correo"
         value={email}
         onChange={(e) => setEmail(e.target.value)}
       />
       <input
         type="password"
-        className="p-2 mb-4 border rounded w-64"
+        className="p-2 mb-4 border rounded w-80"
         placeholder="Contraseña"
         value={password}
         onChange={(e) => setPassword(e.target.value)}
       />
-      <button className="bg-blue-500 text-white px-4 py-2 rounded mb-4" onClick={handleLogin}>
+      <button
+        className="bg-blue-500 text-white px-4 py-2 rounded mb-4"
+        onClick={handleLogin}
+      >
         Login
       </button>
       <button className="text-blue-500 underline" onClick={onRegisterClick}>
